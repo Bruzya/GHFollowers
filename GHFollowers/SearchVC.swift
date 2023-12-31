d4225a7fa363cc3f86b48c89a7c103974f724812
Evgenii Mazrukho <mazrukho@gmail.com>
2023-12-31 13:51:35 +0300

    add Navigation

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Evgenii Mazrukho on 31.12.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
