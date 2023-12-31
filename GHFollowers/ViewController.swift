98d30c55073462cb45447d59c842c17cb0a14878
Evgenii Mazrukho <mazrukho@gmail.com>
2023-12-31 13:26:14 +0300

    add files

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Evgenii Mazrukho on 31.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
