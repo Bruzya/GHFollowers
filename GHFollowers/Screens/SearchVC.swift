f7ae2b334ca9c9c5e4e109ceaa9831f3e71c1a1a
Evgenii Mazrukho <mazrukho@gmail.com>
2024-01-13 16:23:03 +0300

    add DeviceTypes, change UI based on it (especially for iPhone SE)

--- GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/Screens/SearchVC.swift
@@ -10,18 +10,25 @@ import UIKit
 class SearchVC: UIViewController {
 
     //MARK: - UI
+    
     let logoImageView = UIImageView()
     let usernameTextField = GFTextField()
     let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
     
+    
     //MARK: - Properties
+    
     var isUsernameEntered: Bool {
         return !usernameTextField.text!.isEmpty
     }
     
+    var logoImageViewTopConstraint: NSLayoutConstraint!
+    
     //MARK: - Lifecycle
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        usernameTextField.text = ""
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
@@ -36,30 +43,32 @@ class SearchVC: UIViewController {
     }
     
     func createDismissKeyboardTapGesture() {
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
     
     @objc func pushFollowerListVC() {
-        
         guard isUsernameEntered else {
             presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😀.", buttonTitle: "Ok")
             return
         }
-        let followerListVC = FollowersListVC()
-        followerListVC.username = usernameTextField.text
-        followerListVC.title = usernameTextField.text
+        
+        usernameTextField.resignFirstResponder()
+        
+        let followerListVC = FollowersListVC(username: usernameTextField.text!)
         navigationController?.pushViewController(followerListVC, animated: true)
     }
     
     func configureLogoImageView() {
         view.addSubview(logoImageView)
-        
-        logoImageView.image = UIImage(named: "gh-logo")
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = Images.ghLogo
+        
+        let topConstraintConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 20 : 80
+        logoImageViewTopConstraint = logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant)
+        logoImageViewTopConstraint.isActive = true
         
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
--- GHFollowers/Utilities/Constants.swift
+++ GHFollowers/Utilities/Constants.swift
@@ -5,7 +5,7 @@
 //  Created by Evgenii Mazrukho on 05.01.2024.
 //
 
-import Foundation
+import UIKit
 
 enum SFSymbols {
     static let location = "mappin.and.ellipse"
@@ -13,4 +13,37 @@ enum SFSymbols {
     static let gists = "text.alignleft"
     static let following = "person.2"
     static let followers = "heart"
+}
+
+enum Images {
+    static let ghLogo = UIImage(named: "gh-logo")
+}
+
+
+//MARK: - Screen size & Device Types
+
+enum ScreenSize {
+    static let width = UIScreen().bounds.size.width
+    static let height = UIScreen().bounds.size.height
+    static let maxLength = max(ScreenSize.width, ScreenSize.height)
+    static let minLength = min(ScreenSize.width, ScreenSize.height)
+}
+
+enum DeviceTypes {
+    static let idiom = UIDevice.current.userInterfaceIdiom
+    static let nativeScale = UIScreen().nativeScale
+    static let scale = UIScreen().scale
+    
+    static let isiPhoneSE = idiom == .phone && ScreenSize.maxLength == 568.0
+    static let isiPhone8Standart = idiom == .phone && ScreenSize.maxLength == 667.0 && nativeScale == scale
+    static let isiPhone8Zoomed = idiom == .phone && ScreenSize.maxLength == 667.0 && nativeScale > scale
+    static let isiPhone8PlusStandart = idiom == .phone && ScreenSize.maxLength == 736.0
+    static let isiPhone8PlusZoomed = idiom == .phone && ScreenSize.maxLength == 736.0 && nativeScale < scale
+    static let isiPhoneX = idiom == .phone && ScreenSize.maxLength == 812.0
+    static let isiPhoneXsMaxAndXr = idiom == .phone && ScreenSize.maxLength == 896.0
+    static let isiPad = idiom == .pad && ScreenSize.maxLength >= 1024.0
+    
+    static func isiPhoneXAspectRatio() -> Bool {
+        isiPhoneX || isiPhoneXsMaxAndXr
+    }
 }
